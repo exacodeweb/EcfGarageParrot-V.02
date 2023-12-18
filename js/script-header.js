990008d5169d2667e8b48bf7a584282a93ab2454
Franck Giet <franck.giet@gmail.com>
2023-12-18 13:49:08 +0100

    depot projet EcfGarageParrot-V.02

---
+++ js/script-header.js
@@ -0,0 +1,20 @@
+
+function classToggle() {
+  const navs = document.querySelectorAll('.Navbar__Items')
+  
+  navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+}
+
+document.querySelector('.Navbar__Link-toggle')
+  .addEventListener('click', classToggle);
+
+  /*------------------------------------------------------------------------------------------------------------------*//*
+  function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventL
+    */
